7afdc3b16e64633f2d33997a94f8a6c88f723419
DariuszPakulski2137 <89033390+DariuszPakulski2137@users.noreply.github.com>
2024-02-25 15:38:25 +0100

    ok

--- main.py
+++ main.py
@@ -33,37 +33,33 @@ def getIndexOfCoin(coin_name: str):
 # 1 interval = 15s
 def checkIfPriceWentUp(coin_name: str, intervals: int, min_price_change_percent: float):
     id = getIndexOfCoin(coin_name)
-    try:
-        current_price = prices[id]["data"]["current_price"]
-        if len(prices[id]["data"]["price_history"]) >= intervals + 1:
-            historic_price = prices[id]["data"]["price_history"][-1 * intervals]["price"]
-        else:
-            historic_price = prices[id]["data"]["price_history"][0]["price"]
-        if current_price > historic_price:
-            price_change = (current_price / historic_price * 100) - 100
-            price_change = float("{:.3f}".format(price_change))
-            notification = (f"======================\n"
-                            f"{coin_name}\n💹{price_change}%\n{historic_price}$ => {current_price}$\n"
-                            f"since {prices[id]['data']['price_history'][-1 * intervals]['timestamp']}\n"
-                            f"======================")
-            if price_change >= min_price_change_percent:
-                sendTelegramNotification(notification)
+    current_price = prices[id]["data"]["current_price"]
+    if len(prices[id]["data"]["price_history"]) >= intervals + 1:
+        historic_price = prices[id]["data"]["price_history"][-1 * intervals]["price"]
+    else:
+        historic_price = prices[id]["data"]["price_history"][0]["price"]
+    if current_price > historic_price:
+        price_change = (current_price / historic_price * 100) - 100
+        price_change = float("{:.3f}".format(price_change))
+        notification = (f"======================\n"
+                        f"{coin_name}\n💹{price_change}%\n{historic_price}$ => {current_price}$\n"
+                        f"since {prices[id]['data']['price_history'][-1 * intervals]['timestamp']}\n"
+                        f"======================")
+        if price_change >= min_price_change_percent:
+            sendTelegramNotification(notification)
 
-            print(notification)
-        elif current_price < historic_price:
-            price_change = 100 - (current_price / historic_price * 100)
-            price_change = float("{:.3f}".format(price_change))
-            notification = (f"======================\n"
-                            f"{coin_name}\n📉{price_change}%\n{historic_price}$ => {current_price}$\n"
-                            f"since {prices[id]['data']['price_history'][-1 * intervals]['timestamp']}\n"
-                            f"======================")
-            if price_change >= min_price_change_percent:
-                sendTelegramNotification(notification)
-                
-            print(notification)
-    except:
-        x = 0
-        # print("Not enough historicals yet")
+        print(notification)
+    elif current_price < historic_price:
+        price_change = 100 - (current_price / historic_price * 100)
+        price_change = float("{:.3f}".format(price_change))
+        notification = (f"======================\n"
+                        f"{coin_name}\n📉{price_change}%\n{historic_price}$ => {current_price}$\n"
+                        f"since {prices[id]['data']['price_history'][-1 * intervals]['timestamp']}\n"
+                        f"======================")
+        if price_change >= min_price_change_percent:
+            sendTelegramNotification(notification)
+
+        print(notification)
 
 
 # add saving to file
