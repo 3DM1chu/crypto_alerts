5037b2633ef0470aea015159954adfe4f8e4ad62
DariuszPakulski2137 <89033390+DariuszPakulski2137@users.noreply.github.com>
2024-02-29 10:47:34 +0100

    Binance new version V3, with async http requests

--- main.py
+++ main.py
@@ -245,7 +245,7 @@ async def fetch_coin_price(session, coin, semaphore):
             current_price = float(coin_data[4])
             token = tokens[getIndexOfCoin(coin["symbol"])]
             token.addPriceEntry(current_price, datetime.now())
-            print(f"{coin_data}")
+            #print(f"{coin_data}")
     except:
         print("ok, skip")
     semaphore.release()
